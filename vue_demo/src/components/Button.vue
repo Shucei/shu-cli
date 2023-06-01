cac67d7e5b051d2d31b8f08e2531813295924606
Shucei <1003823477@qq.com>
2023-06-01 17:52:18 +0800

    feat:从github拉取文件创建到本地&创建组件

---
+++ vue_demo/src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Button">
+    <h1 class="Button">Button</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, ref } from "vue";
+const message = ref("Hello World");
+</script>
+
+<style scoped>
+
+</style>
